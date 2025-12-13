a739d08a2cc36ee85dba93701939244b51545e37
CodeByAkhilesh <dinoak308@gmail.com>
2025-12-13 11:11:22 +0530

    Add services in service section

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const ServiceCard = () => {
+const ServiceCard = ({serviceName, discription, index}) => {
   return (
-    <div className='relative'>
-        <div className='h-[385px] w-[291px] border border-[#268eff] rounded-2xl bg-black absolute top-0 z-10'></div>
-        <div className='h-[385px] w-[291px] border border-[#268eff] rounded-2xl -rotate-3 bg-gradient-to-r from-[#3aab50] to-[#268eff]'></div>
+    <div key={index} className='relative'>
+        <div className='h-[385px] w-[291px] border border-[#268eff] rounded-2xl bg-black absolute top-0 z-10 px-7 py-10'>
+          <h1 className='text-white text-4xl'>{serviceName}</h1>
+          <p className='text-white text-xl font-medium tracking-wide pt-4'>{discription}</p>
+        </div>
+        <div className='h-[385px] w-[291px] border border-[#268eff] rounded-2xl -rotate-3 bg-linear-to-r from-[#3aab50] to-[#268eff]'></div>
     </div>
   )
 }
--- src/components/pages/Services.jsx
+++ src/components/pages/Services.jsx
@@ -1,19 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ServiceCard from '../ServiceCard'
 
 const Services = () => {
+
+const [serviceContent, setServiceContent] = useState([
+    {
+      servicName:"Fund Selection Support",
+      discription:"We assist investors in selection of mutual fund schemes based on their financial goals and risk profile."
+    },
+    {
+      servicName:"Portfolio Management & Reviwe",
+      discription:"We assist investors in selection of mutual fund schemes based on their financial goals and risk profile."
+    },
+    {
+      servicName:"Goal-Based Investment",
+      discription:"We assist investors in selection of mutual fund schemes based on their financial goals and risk profile."
+    },
+    {
+      servicName:"Goal-Based Investment",
+      discription:"We assist investors in selection of mutual fund schemes based on their financial goals and risk profile."
+    },
+    {
+      servicName:"Goal-Based Investment",
+      discription:"We assist investors in selection of mutual fund schemes based on their financial goals and risk profile."
+    },
+    {
+      servicName:"Goal-Based Investment",
+      discription:"We assist investors in selection of mutual fund schemes based on their financial goals and risk profile."
+    },
+  ]);
+
   return (
     <div className=''>
     <div className='w-[1210px] mx-auto text-6xl font-bold tracking-tight mt-20'>
-      <h1 className='text-white'>Committed to helping you achieve <br /> <span className='bg-gradient-to-r from-[#3aab50] to-[#268eff] bg-clip-text text-transparent'> Financial Freedom</span></h1>
+      <h1 className='text-white'>Committed to helping you achieve <br /> <span className='bg-linear-to-r from-[#3aab50] to-[#268eff] bg-clip-text text-transparent'> Financial Freedom</span></h1>
       <h2 className='text-white text-5xl font-medium mt-10 text-center'>How We Support Your Investments</h2>
-      <div className='py-15 flex flex-wrap gap-10 items-center justify-center '>
-      <ServiceCard/>
-      <ServiceCard/>
-      <ServiceCard/>
-      <ServiceCard/>
-      <ServiceCard/>
-      <ServiceCard/>
+      <div className='py-15 flex flex-wrap gap-20 justify-center '>
+       {serviceContent.map((content, index)=>{
+        return(
+        <div key={index}>
+        <ServiceCard serviceName={content.servicName} discription={content.discription} />
+        </div>
+       )})}
       </div>
     </div>
     </div>
