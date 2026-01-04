5829efe3f37b70e88c3ea0035d4b6ddf9a3b714f
CodeByAkhilesh <dinoak308@gmail.com>
2026-01-04 17:15:25 +0530

    style: add initial Contact page layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,47 +7,66 @@ function Navbar() {
 
   return (
     <div className="fixed z-30 w-full bg-[#00000000] backdrop-blur-xl">
-    <nav className="text-white md:max-w-302.5 md:px-10 lg:px-5 px-10 py-4 flex justify-between items-center w-full mx-auto">
-      {/* Logo */}
-      <div className="text-2xl font-bold w-20">
-        <img
-          className="object-cover h-full w-full "
-          src="src\assets\brand-logo-web.png"
-          alt="Logo"
-        />
-      </div>
+      <nav className="text-white md:max-w-302.5 md:px-10 lg:px-5 px-10 py-4 flex justify-between items-center w-full mx-auto">
+        {/* Logo */}
+        <div className="text-2xl font-bold w-20">
+          <img
+            className="object-cover h-full w-full "
+            src="src\assets\brand-logo-web.png"
+            alt="Logo"
+          />
+        </div>
 
-      {/* Desktop Menu */}
-      <nav className="hidden lg:flex md:gap-6 lg:gap-8 text-lg items-center">
-        <NavLink to="/" className="cursor-pointer hover:text-blue-400 font-bold">Home</NavLink>
-        <NavLink to="/Services" className="cursor-pointer hover:text-blue-400 font-bold">
-          Services
-        </NavLink>
-        <NavLink to="/about" className="cursor-pointer hover:text-blue-400 font-bold">About</NavLink>
-        <NavLink to="/calculator" className="cursor-pointer hover:text-blue-400 font-bold">
-          Calculator
-        </NavLink>
-        <NavLink to="contact" className="cursor-pointer hover:text-blue-400 font-bold">
-          Contact
-        </NavLink>
-        <Button btnName={"Login"} />
+        {/* Desktop Menu */}
+        <nav className="hidden lg:flex md:gap-6 lg:gap-8 text-lg items-center">
+          <NavLink
+            to="/"
+            className="cursor-pointer hover:text-blue-400 font-bold"
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/Services"
+            className="cursor-pointer hover:text-blue-400 font-bold"
+          >
+            Services
+          </NavLink>
+          <NavLink
+            to="/about"
+            className="cursor-pointer hover:text-blue-400 font-bold"
+          >
+            About
+          </NavLink>
+          <NavLink
+            to="/calculator"
+            className="cursor-pointer hover:text-blue-400 font-bold"
+          >
+            Calculator
+          </NavLink>
+          <NavLink
+            to="/contact"
+            className="cursor-pointer hover:text-blue-400 font-bold"
+          >
+            Contact
+          </NavLink>
+          <Button btnName={"Login"} />
+        </nav>
+
+        {/* Mobile Menu Button */}
+        <button className="lg:hidden text-3xl" onClick={() => setOpen(!open)}>
+          ☰
+        </button>
+
+        {/* Mobile Menu */}
+        {open && (
+          <ul className="absolute top-16 left-0 w-full bg-[#030202a8] flex flex-col gap-4 py-6 text-center lg:hidden text-lg backdrop-blur-2xl">
+            <li className="cursor-pointer hover:text-blue-400">Home</li>
+            <li className="cursor-pointer hover:text-blue-400">About</li>
+            <li className="cursor-pointer hover:text-blue-400">Services</li>
+            <li className="cursor-pointer hover:text-blue-400">Contact</li>
+          </ul>
+        )}
       </nav>
-
-      {/* Mobile Menu Button */}
-      <button className="lg:hidden text-3xl" onClick={() => setOpen(!open)}>
-        ☰
-      </button>
-
-      {/* Mobile Menu */}
-      {open && (
-        <ul className="absolute top-16 left-0 w-full bg-[#030202a8] flex flex-col gap-4 py-6 text-center lg:hidden text-lg backdrop-blur-2xl">
-          <li className="cursor-pointer hover:text-blue-400">Home</li>
-          <li className="cursor-pointer hover:text-blue-400">About</li>
-          <li className="cursor-pointer hover:text-blue-400">Services</li>
-          <li className="cursor-pointer hover:text-blue-400">Contact</li>
-        </ul>
-      )}
-    </nav>
     </div>
   );
 }
--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
+import Button from "../Button";
+import Navbar from "../Navbar";
+import Footer from "../Footer";
 
 const Contact = () => {
   return (
     <div>
-      
+      <Navbar />
+      <div className="w-280 mx-auto pt-0.5 min-h-screen">
+        <div className="border border-[#268eff] h-117 w-full text-white rounded-2xl overflow-hidden flex justify-around mt-35">
+          <img
+            src="https://images.leadconnectorhq.com/image/f_webp/q_80/r_1200/u_https://assets.cdn.filesafe.space/Rtb2945nBeIor8lTKMZj/media/ee1fd369-bcfd-4b15-b8c5-1ee1067ddbfe.png"
+            alt=""
+          />
+          <div className="left-content flex flex-col justify-center w-1/2 ">
+            <h3 className="font-bold text-3xl text-[#268eff]">Contact Us</h3>
+            <p>To get in touch, please use the details or references provided below. I will respond as soon as possible.</p>
+            <h4 className="text-2xl font-bold pt-4">+91-7232933333</h4>
+            <h4 className="text-2xl font-bold pt-4">Niveshonefs@gmail.com</h4>
+            <div className="pt-5">
+              <Button btnName={"Chat on whatsapp"} />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer/>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
