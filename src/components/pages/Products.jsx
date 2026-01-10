b4018b6bc4263bab14ec64615a4d393fa1633bbc
CodeByAkhilesh <dinoak308@gmail.com>
2026-01-10 13:08:09 +0530

    feat(product-section): update content and make layout responsiv

--- src/components/pages/Products.jsx
+++ src/components/pages/Products.jsx
@@ -7,50 +7,50 @@ function Products() {
 
    const [goalsData, setGoalsData] = useState([
       {
-        img: "https://images.leadconnectorhq.com/image/f_webp/q_80/r_900/u_https://assets.cdn.filesafe.space/eSL9Lyo3BED9dy8wd3Pb/media/31bfa3fa-ac70-4297-8bc4-c563bf6771c7.png",
-        headding: "Own House",
-        pera: "Have a dream of owning your own house? Achieve that goal fast with the right financial plan.",
+        img: "src/assets/Product-img/mutual-fund.png",
+        headding: "Mutual Fund",
+        pera: "Mutual funds are smart investment options designed to build long-term wealth and achieve financial goals efficiently.",
       },
       {
-        img: "https://images.leadconnectorhq.com/image/f_webp/q_80/r_1200/u_https://assets.cdn.filesafe.space/eSL9Lyo3BED9dy8wd3Pb/media/bc53ae9a-2e80-4062-8d32-f8333bf9f391.png",
-        headding: "Dream Car",
-        pera: "Desiring to get your dream car? Invest in the right places to get your dream car sooner.",
+        img: "src/assets/Product-img/NFOs.png",
+        headding: "NFO",
+        pera: "New Fund Offer allows investors to invest early in newly launched mutual fund schemes at initial prices.",
       },
       {
-        img: "https://images.leadconnectorhq.com/image/f_webp/q_80/r_1200/u_https://assets.cdn.filesafe.space/eSL9Lyo3BED9dy8wd3Pb/media/b50a54f9-647d-40c0-9e90-85d46d1c2787.png",
-        headding: "Vacation",
-        pera: "Vacation doesn't have to be just a dream. Enjoy your vacation without breaking your savings.",
+        img: "src/assets/Product-img/SIP.png",
+        headding: "SIP",
+        pera: "Systematic Investment Plan allows investors to invest a fixed amount regularly in mutual funds for disciplined wealth growth.",
       },
       {
-        img: "https://images.leadconnectorhq.com/image/f_webp/q_80/r_1200/u_https://assets.cdn.filesafe.space/eSL9Lyo3BED9dy8wd3Pb/media/7181b4b5-7927-41e2-865f-c8acde09f7f3.png",
-        headding: "Kids' Education",
-        pera: "We all want the best for our Kids. Finances shouldn't stop them from pursuing their career.",
+        img: "src/assets/Product-img/SWP.png",
+        headding: "SWP",
+        pera: "Systematic Withdrawal Plan lets investors withdraw a fixed amount regularly from their mutual fund investments.",
       },
       {
-        img: "https://images.leadconnectorhq.com/image/f_webp/q_80/r_1200/u_https://assets.cdn.filesafe.space/eSL9Lyo3BED9dy8wd3Pb/media/e19f04f6-d6bf-4809-bd0d-f240f152f7cc.png",
-        headding: "Happy Retirement",
-        pera: "Getting close to retirement age? Plan your finances accordingly to retire early and happily.",
+        img: "src/assets/Product-img/LAMF.png",
+        headding: "Loan Against MF",
+        pera: "Investors can avail a loan by pledging their mutual fund units, without redeeming their investments.",
       },
       {
-        img: "https://images.leadconnectorhq.com/image/f_webp/q_80/r_1200/u_https://assets.cdn.filesafe.space/eSL9Lyo3BED9dy8wd3Pb/media/afbc5965-c1ce-4c1b-bfb1-c440ef1ada4b.png",
-        headding: "Childrens' Wedding",
-        pera: "With the right financial plan, you don't have to sacrifice on the budget for your kids' wedding.",
+        img: "src/assets/Product-img/FD.png",
+        headding: "Fixed Deposits",
+        pera: "Corporate Fixed Deposits are investment options offering fixed returns over a period, issued by companies.",
       },
     ]);
 
   return (
-    <div className=''>
+    <div className='overflow-'>
      <Navbar/>
-     <div className='min-h-screen w-302.5 mx-auto'>
-      <h1 className="text-3xl md:text-4xl mx-5 xl:w-302.5 lg:text-6xl font-bold text-white mb-10 pt-35">
+     <div className='min-h-screen xl:w-302.5 mx-auto'>
+      <h1 className="text-3xl md:text-4xl mx-5 md:mx-10 xl:mx-5 lg:mx-25 xl:w-302.5 lg:text-6xl font-bold text-white pt-28 md:pt-35">
           Wealth Building {" "}
           <span className="bg-linear-to-r from-[#3aab50] to-[#268eff] bg-clip-text text-transparent">
             Products.
           </span>
         </h1>
-        <div className='flex justify-center flex-wrap gap-15 mt-15 mb-20'>
+        <div className='flex flex-col md:flex-row justify-center flex-wrap items-center gap-15 mt-10 md:mt-15 mb-30'>
           {goalsData.map((data, index) => {
-          return <GoalsCard key={index}  />;
+          return <GoalsCard key={index} goalsData={data}  />;
         })}
         </div>
 
