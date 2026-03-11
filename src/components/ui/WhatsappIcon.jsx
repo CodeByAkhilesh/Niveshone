bc4704750d8d53cf76c844bb07d31e9d2a819acf
CodeByAkhilesh <dinoak308@gmail.com>
2026-03-11 17:48:07 +0530

    fix: adjust WhatsApp icon position

--- src/components/ui/WhatsappIcon.jsx
+++ src/components/ui/WhatsappIcon.jsx
@@ -6,7 +6,7 @@ const WhatsappIcon = () => {
     <a
       href="https://wa.me/917984300926?text=Hi%20Nivesh%20One"
       target="_blank"
-      className="whatsappIcon bg-green-500 w-12 h-12 flex items-center justify-center rounded-full fixed bottom-6 right-10 md:right-13 lg:right-13 hover:scale-110 transition z-40"
+      className="whatsappIcon bg-green-500 w-12 h-12 flex items-center justify-center rounded-full fixed bottom-6 right-3 md:right-5 lg:right-13 hover:scale-110 transition z-40"
     >
       <RiWhatsappLine />
     </a>
